4527d14e8b9eb9393ec38f267e90de6da31d1671
GO-Sushi <deviljinX972@gmail.com>
2021-03-11 16:27:07 -0400

    fonctions de contacts

--- app/Controllers/API.php
+++ app/Controllers/API.php
@@ -1,14 +1,108 @@
-<?php
-
+<?php 
 namespace App\Controllers;
-use CodeIgniter\Model;
+use App\Controllers\BaseController;
+use App\Models\ContactModel;
 
-class API extends BaseController{
+class Api extends BaseController{
 
+    public $contactModel = null;
+
+    public function __construct(){
+
+		$this->contactModel = new ContactModel();
+    
+    }
+
+    //fonction de listage + pagination et recherche
     public function index(){
 
+        $listecontacts=$this->contactModel->paginate(10);
+         
+        //
+        if(!empty($this->request->getVar("type")) && !empty($this->request->getVar("elementRecherche"))){
+            $type=$this->request->getVar("type");
+            $elementRecherche=$this->request->getVar("elementRecherche");
+            
+            switch ($type){
 
+                case "recherche":
+                    //dd($listecontacts);
+                $listecontacts=$this->contactModel->like( "first_Name",$elementRecherche ,'both',null,true)
+                                                  ->orlike("last_Name",$elementRecherche ,'both',null,true)
+                                                  ->orderBy('id', 'ASC')->paginate(10);
 
-
+                break;
+            }
         }
+        //conversion du type originel de listcontacts -> le type JSON
+        return $this->response->setJSON($listecontacts);
+    }
+    
+    //fonction de modification et d'ajout
+    public function edit(){
+
+
+
+
+    }
+
+    //fonction de suppression simple et multiple     
+    public function delete(){
+
+            //.1 je recupere l'id du contacte a supprimer
+            $id=$this->request->getVar('id');
+            $etatAction=['response'=>false];
+            //.2 si il existe je passe au 3
+            if(!empty($id)){
+
+                //.3 je fait ma requete pour supprimer le contacte
+                $this->contactModel->where('id',$id)->delete();
+
+                //.4 je vérifie que la suppression a bien été faite
+
+                
+                $etatAction=['response'=>true];
+                
+            }
+           
+         return $this->response->setJSON($etatAction);
+        
+    }
+
+    //fonction de mise en favoris des contacts
+    // utilise l'id à ajouter en paramètre
+    public function fav(){
+
+            //1.je recupere l'id du contacte a ajouter ou a supprimer des favoris
+        $id=$this->request->getVar('id');
+
+            //2.je vérifie l'existence de ce contacte et si il existe je passe au 3
+            if(!empty($id)){
+
+            //3.j'interroge la base de donnée pour savoir si il est deja favoris ou non grace a une requête
+            $contact=$this->contactModel->where('id',$id)->first();
+            $etatAction=['response'=>false];
+            
+            //4.si il est favoris je le retire sinon je l'ajoute
+            if(!empty($contact)){
+                
+                //DEBUT test favoris (YES)
+                if($contact['favory'] == 'Yes'){
+                    $this->contactModel->where('id',$id)->set('favory','NO')->update();
+                    
+                  
+                //END test favoris (YES) 
+                }else{
+                    $this->contactModel->where('id',$id)->set('favory','Yes')->update();
+                    
+                }   
+                 
+            }
+
+            //5.je vérifie que ma requete a bien été faite
+               $etatAction=['response'=>true]; 
+        }
+             return $this->response->setJSON($etatAction);
+
+    }
 }
